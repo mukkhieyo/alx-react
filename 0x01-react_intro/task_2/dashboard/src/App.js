50eeef9fe724a453340a01270833d308e8a58058
Mukhtar Audu <99723874+mukkhieyo@users.noreply.github.com>
2024-03-02 12:30:33 +0100

    Add files via upload

---
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,28 @@
+import holberton from '../src/holberton.jpg'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={holberton} alt='' />
+        <h1>School dashboard</h1>
+      </header>
+      <div className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <div>
+            <label for='email'>Email:</label>
+            <input type='email' id='email' />
+            <label for='password'>Password:</label>
+            <input type='password' id='password' />
+            <button>OK</button>
+        </div>
+      </div>
+      <footer className='App-footer'>
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
