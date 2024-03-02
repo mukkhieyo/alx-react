c2df5baac2b77c083b26959a1c041bfc8809508e
Mukhtar Audu <99723874+mukkhieyo@users.noreply.github.com>
2024-03-02 12:17:14 +0100

    Add files via upload

---
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import Notifications from '../Notifications/Notifications'
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import CourseList from '../CourseList/CourseList'
+import Footer from '../Footer/Footer'
+import "./App.css";
+
+function App({isLoggedIn}) {
+  return (
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        {isLoggedIn ? <CourseList /> : <Login />}
+        <Footer />
+      </div>
+    </React.Fragment>
+  );
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+}
+
+App.defaultProps = {
+  isLoggedIn: false,
+}
+
+export default App;
---
+++ 0x02-react_props/task_4/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CourseListRow = ({ isHeader, textFirstCell, textSecondCell }) => {
+    return (
+        <tr>
+            {isHeader ? 
+                (textSecondCell == null ? 
+                    (<th colSpan={2}>{textFirstCell}</th>)
+                    : (
+                        <>
+                            <th>{textFirstCell}</th>
+                            <th>{textSecondCell}</th>
+                        </>
+                    )) : (
+                        <>
+                            <td>{textFirstCell}</td>
+                            <td>{textSecondCell}</td>
+                        </>
+                    )}
+        </tr>
+    )
+}
+
+CourseListRow.PropTypes = {
+    isHeader: PropTypes.bool,
+    textFirstCell: PropTypes.string.isRequired,
+    textSecondCell: PropTypes.string
+}
+
+CourseListRow.defaultProps = {
+    isHeader: false,
+    textSecondCell: null,
+  };
+
+export default CourseListRow
